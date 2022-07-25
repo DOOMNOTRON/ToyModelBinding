9855cd17e1ec7eeb1c12d8f9d7bc7e73e2bab544
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-07-25 01:15:32 -0700

    Removed the * from the form. Added validation to the Toy class.

--- ToyModelBinding/Models/Toy.cs
+++ ToyModelBinding/Models/Toy.cs
@@ -1,4 +1,6 @@
-﻿namespace ToyModelBinding.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToyModelBinding.Models
 {
 
     // Validate Data
@@ -11,16 +13,24 @@
         /// <summary>
         /// Represents the name of the toy
         /// </summary>
-        public string Title { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Name = "Product Name")]
+        public string Title { get; set; } // add ? next to type to make nullable
 
         /// <summary>
         /// Represents the MSRP of the Toy
         /// </summary>
+        [Required(ErrorMessage = "This field is required.")]//does not show up.
+        [Display(Name = "MSRP")]
+        [Range(0, double.MaxValue, ErrorMessage = "MSRP must be positive")]
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
 
         /// <summary>
         /// Represents the current Quantity in stock
         /// </summary>
+        [Range(0, double.MaxValue, ErrorMessage = "Quantity must be positive")]
+        [Required(ErrorMessage = "This field is required.")]
         public int Quantity { get; set; }
     }
 }
