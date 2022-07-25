b6a62cc287bc1a23cbefe527b55241b5dd1ffe11
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-07-25 00:11:27 -0700

    Added a new li item for creata a product and a new view for Add a product. Created a new model for Class Toy and a Product controller.

---
+++ ToyModelBinding/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToyModelBinding.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: /Product/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]// THis action is hit after the user submits form data
+        public IActionResult Create(IFormCollection formData)
+        {
+            // Validate data
+
+            
+
+            // Add to database
+
+            //return a view
+            return RedirectToActionPermanent("Index", "Home");
+
+        }
+    }
+}
---
+++ ToyModelBinding/Models/Toy.cs
@@ -0,0 +1,26 @@
+﻿namespace ToyModelBinding.Models
+{
+
+    // Validate Data
+
+    /// <summary>
+    /// Represent a toy to be added
+    /// </summary>
+    public class Toy
+    {
+        /// <summary>
+        /// Represents the name of the toy
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Represents the MSRP of the Toy
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Represents the current Quantity in stock
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
